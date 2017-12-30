7308f53890b70d7fbfcfd5a705d3405307588022
John Kor <john.kor@gmail.com>
2017-12-30 11:34:04 -0800

    basic setup done!
    however i couldnt get the latest version of react (16.*) to work with rollup. had to downgrade to 15.*

---
+++ src/components/address-book.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export class AddressBook extends React.Component {
+  render() {
+    return (
+      <div>
+        hi there
+      </div>
+    )
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import {AddressBook} from './components/address-book'
+
+ReactDOM.render(
+  <AddressBook />,
+  document.getElementById('app'),
+)
